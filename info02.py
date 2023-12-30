1761341550e72be03f473de59a1ada67c37fbc33
Nico Mengisen <mengisennico@gmail.com>
2023-12-30 22:50:35 +0100

    Update

--- info02.py
+++ info02.py
@@ -3,151 +3,195 @@
 # PRO DB PY
 
 import tkinter as tk
+from customtkinter import *
 import random
 from math import pow
 import time
 import database
 import datetime
-from tkinter.messagebox import *
+from tkinter.messagebox import showerror
 
-#important data (to save)
-pseudo="Gaston" #provisory pseudo for user
-exercise="INFO02"
-nbtrials=0 #number of total trials
-nbsuccess=0 #number of successfull trials
+class Info02Game(CTkFrame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
 
-# Liaison entre le canvas et le code
-unite = ["B", "kB", "MB", "GB", "TB"]
-n1 = 0  # valeur à convertir
-u1 = unite[0]
-n2 = 0  # valeur à convertir
-u2 = unite[0]
-rapport = 0
+        # Important data (to save)
+        self.pseudo = "Gaston"  # Provisional pseudo for the user
+        self.exercise = "INFO02"
+        self.nbtrials = 0  # Number of total trials
+        self.nbsuccess = 0  # Number of successful trials
 
+        # Liaison entre le canvas et le code
+        self.unite = ["B", "kB", "MB", "GB", "TB"]
+        self.n1 = 0  # Valeur à convertir
+        self.u1 = self.unite[0]
+        self.n2 = 0  # Valeur à convertir
+        self.u2 = self.unite[0]
+        self.rapport = 0
+        self.start_date = datetime.datetime.now()
 
-def next(event):
-    global n1, u1, u2, rapport
-    window_info02.configure(bg=hex_color)
-
-    n1 = round(random.random(), 3)
-    dec = random.randint(0, 3)
-    for i in range(dec):
-        n1 *= 10
-    n1 = round(n1, 3)
-    p1 = random.randint(1, 4)
-    u1 = unite[p1]
-    p2 = p1
-    while p1 == p2:
-        p2 = random.randint(0, 4)
-    u2 = unite[p2]
-    rapport = pow(10, 3 * (p2 - p1))
-    label_n1.configure(text=f" {n1} {u1} =")
-    label_u2.configure(text=f" {u2} ")
-    entry_n2.delete(0, 'end')
+        self.configure(fg_color='white')  # Set the foreground color
+        self.grid_columnconfigure((0, 1, 2), weight=1)
+        self.create_widgets()
+        self.next()
 
 
-def save_game(event):
-    global start_date, pseudo, exercise, nbtrials, nbsuccess
+    def create_widgets(self):
+        # Title label
+        self.title_label = CTkLabel(self, text=f"{self.exercise}", font=("Arial", 15))
+        self.title_label.grid(row=0, column=0, columnspan=3, ipady=5, padx=20, pady=20)
 
-    # pour obtenir le temp dernier
-    end_time = datetime.datetime.now()
+        # Timer label
+        self.duration_label = CTkLabel(self, text="0:00", font=("Arial", 15))
+        self.duration_label.grid(row=0, column=2, ipady=5, padx=10, pady=10)
 
-    # pour calculer le temps de partie
-    duration_seconds = (end_time - start_date).total_seconds()
+        # Pseudo label and entry
+        CTkLabel(self, text='Pseudo:', font=("Arial", 15)).grid(row=1, column=0, padx=5, pady=5)
+        self.pseudo_entry = CTkEntry(self, font=("Arial", 15))
+        self.pseudo_entry.grid(row=1, column=1)
 
-    pseudo = pseudo_entry.get()
+        # Result label
+        self.lbl_result = CTkLabel(self, text=f"{self.pseudo} Essais réussis : 0/0", font=("Arial", 15))
+        self.lbl_result.grid(row=1, column=2, columnspan=3, ipady=5, padx=20, pady=20)
 
-    # insert bd
-    database.save_game_result(pseudo, exercise, duration_seconds, nbtrials, nbsuccess)
+        # Conversion question labels and entry
+        self.label_n1 = CTkLabel(self, text="n1:", font=("Arial", 15))
+        self.label_n1.grid(row=2, column=0, ipady=5, padx=20, pady=20, sticky='E')
 
-    window_info02.destroy()
+        self.entry_n2 = CTkEntry(self, font=("Arial", 15))
+        self.entry_n2.grid(row=2, column=1, ipady=5, padx=5, pady=20, sticky='E')
 
-def test(event):
-    global n2, nbsuccess, nbtrials, pseudo_entry
-    # Fonction pour tester si la valeur est juste
-    n2 = float(entry_n2.get().replace(" ", ""))
-    nbtrials+=1
-    success = (abs(n1 / n2 / rapport - 1) < 0.01) #tolerance 1%
-    if success:
-        nbsuccess += 1
-        window_info02.configure(bg="green")
-    else:
-        window_info02.configure(bg="red")
+        self.label_u2 = CTkLabel(self, text="u2:", font=("Arial", 15))
+        self.label_u2.grid(row=2, column=2, ipady=5, padx=5, pady=20, sticky='W')
 
-    pseudo = pseudo_entry.get()
-    lbl_result.configure(text=f"{pseudo} Essais réussis : {nbsuccess} / {nbtrials}")
-    window_info02.update()
-    time.sleep(1) # delai 1s
-    next(event=None)
+        # 'Next' button
+        self.btn_next = CTkButton(self, text="Suivant", font=("Arial", 15), command=self.next)
+        self.btn_next.grid(row=3, column=0, columnspan=3, ipady=5, padx=5, pady=5)
 
+        # 'Finish' button
+        self.btn_finish = CTkButton(self, text="Terminer", font=("Arial", 15), command=self.save_game)
+        self.btn_finish.grid(row=4, column=0, columnspan=3, ipady=5, padx=5, pady=5)
 
-def display_timer():
-    duration = datetime.datetime.now() - start_date  # elapsed time since beginning, in time with decimals
-    duration_s = int(duration.total_seconds())  # idem but in seconds (integer)
-    # display min:sec (00:13)
-    duration_label.configure(text="{:02d}".format(int(duration_s / 60)) + ":" + "{:02d}".format(duration_s % 60))
-    window_info02.after(1000, display_timer)  # recommencer après 15 ms
+        # Bind the Return key to the test method (when the user presses Enter after typing their answer)
+        self.entry_n2.bind("<Return>", self.test)
+        self.display_timer()
 
 
-def open_window_info_02(window):
-    global window_info02, duration_label, lbl_result, entry_n2, label_u2, label_n1, hex_color, start_date,pseudo_entry
-    window_info02 = tk.Toplevel(window)
-
-    #window_info02 = tk.Tk()
-    window_info02.title("Conversion d'unités")
-    window_info02.geometry("1100x900")
-    window_info02.grid_columnconfigure((0,1,2), minsize=150, weight=1)
-
-    # color definition
-    rgb_color = (139, 201, 194)
-    hex_color = '#%02x%02x%02x' % rgb_color # translation in hexa
-    window_info02.configure(bg=hex_color)
-
-    title_label = tk.Label(window_info02, text=f"{exercise}", font=("Arial", 15))
-    title_label.grid(row=0,column=0,columnspan=3, ipady=5, padx=20,pady=20)
-    duration_label = tk.Label(window_info02, text="0:00", font=("Arial", 15))
-    duration_label.grid(row=0,column=2, ipady=5, padx=10,pady=10)
-
-    tk.Label(window_info02, text='Pseudo:', font=("Arial", 15)).grid(row=1, column=0, padx=5, pady=5)
-    pseudo_entry = tk.Entry(window_info02, font=("Arial", 15))
-    # pseudo_entry.pack(ipadx=2, ipady=10, padx=5,pady=5)
-    pseudo_entry.grid(row=1, column=1)
+    def next(self):
+        # Logic to set up the next question
+        self.n1 = round(random.uniform(0.001, 1000), 3)
+        u1_index = random.randint(1, 4)
+        self.u1 = self.unite[u1_index]
+        u2_index = random.randint(0, 4)
+        while u1_index == u2_index:
+            u2_index = random.randint(0, 4)
+        self.u2 = self.unite[u2_index]
+        self.rapport = pow(10, 3 * (u2_index - u1_index))
+        self.label_n1.configure(text=f"{self.n1} {self.u1} = ")
+        self.label_u2.configure(text=f"{self.u2}")
+        self.entry_n2.delete(0, tk.END)
+        self.configure(fg_color='white')  # Reset background color
 
 
+    def save_game(self):
+        # Calculate the duration of the game
+        end_time = datetime.datetime.now()
+        duration_seconds = (end_time - self.start_date).total_seconds()
+
+        # Get the pseudo from the entry widget
+        pseudo = self.pseudo_entry.get()
+
+        # Save the game result to the database
+        database.save_game_result(pseudo, self.exercise, duration_seconds, self.nbtrials, self.nbsuccess)
+
+        # Destroy the game frame or hide it, depending on your application's needs
+        self.destroy()
+
+    def test(self, event=None):
+        try:
+            self.n2 = float(self.entry_n2.get().replace(",", "."))
+            self.nbtrials += 1
+            success = abs(self.n1 / self.n2 / self.rapport - 1) < 0.01  # 1% tolerance
+            if success:
+                self.nbsuccess += 1
+                self.configure(fg_color='green')  # Success color
+            else:
+                self.configure(fg_color='red')  # Error color
+            self.lbl_result.configure(text=f"{self.pseudo} Essais réussis : {self.nbsuccess} / {self.nbtrials}")
+            self.after(1000, self.next)  # Wait 1 second then go to next question
+        except ValueError:
+            showerror("Erreur", "Entrée non valide. Veuillez entrer un nombre.")
+            self.entry_n2.delete(0, tk.END)
 
 
-    lbl_result = tk.Label(window_info02, text=f"{pseudo}  Essais réussis : 0/0", font=("Arial", 15))
-    lbl_result.grid( row=1, column=2,columnspan=3, ipady=5, padx=20,pady=20)
-
-    label_n1 = tk.Label(window_info02, text="n1:",font=("Arial", 15))
-    label_n1.grid(row=2,column=0,ipady=5, padx=20,pady=20,sticky='E')
-
-    entry_n2 = tk.Entry(window_info02,font=("Arial", 15))
-    entry_n2.grid(row=2,column=1,ipady=5,padx=5, pady=20,sticky='E')
-
-    label_u2 =tk.Label(window_info02, text="u2:",font=("Arial", 15))
-    label_u2.grid(row=2,column=2,ipady=5,padx=5,pady=20,sticky='W')
-
-    btn_next =tk.Button(window_info02, text="Suivant", font=("Arial", 15))
-    btn_next.grid(row=3,column=0,columnspan=3,ipady=5, padx=5,pady=5)
-
-    btn_finish = tk.Button(window_info02, text="Terminer", font=("Arial", 15))
-    btn_finish.grid(row=6, column=0, columnspan=6)
-
-    # button-1 clic a gauche souris
-    # bind pour relier un function sur un bouton
-    btn_finish.bind("<Button-1>", lambda event: (save_game(event)))
-
-    start_date = datetime.datetime.now()
-    display_timer()
-    # first call of next_point
-    next(event=None)
+    def display_timer(self):
+        # Calculate elapsed time
+        duration = datetime.datetime.now() - self.start_date
+        duration_s = int(duration.total_seconds())
+        
+        # Update the duration label text to display the time in min:sec format
+        self.duration_label.configure(text="{:02d}:{:02d}".format(duration_s // 60, duration_s % 60))
+        
+        # Schedule the timer to update after one second again
+        self.after(1000, self.display_timer)
 
 
-    # binding actions (entry & buttons)
-    entry_n2.bind("<Return>", test)
-    btn_next.bind("<Button-1>", next)
-    btn_finish.bind("<Button-1>", save_game)
+    def open_window_info_02(self, window):
+            global window_info02, duration_label, lbl_result, entry_n2, label_u2, label_n1, hex_color, start_date, pseudo_entry
+            window_info02 = tk.Toplevel(window)
 
-    # Main loop
-    window_info02.mainloop()
+            # window_info02 = tk.Tk()
+            window_info02.title("Conversion d'unités")
+            window_info02.geometry("1100x900")
+            window_info02.grid_columnconfigure((0, 1, 2), minsize=150, weight=1)
+
+            # color definition
+            rgb_color = (139, 201, 194)
+            hex_color = '#%02x%02x%02x' % rgb_color  # translation in hexa
+            window_info02.configure(bg=hex_color)
+
+            title_label = CTkLabel(window_info02, text=f"{exercise}", font=("Arial", 15))
+            title_label.grid(row=0, column=0, columnspan=3, ipady=5, padx=20, pady=20)
+            duration_label = CTkLabel(window_info02, text="0:00", font=("Arial", 15))
+            duration_label.grid(row=0, column=2, ipady=5, padx=10, pady=10)
+
+            CTkLabel(window_info02, text='Pseudo:', font=("Arial", 15)).grid(row=1, column=0, padx=5, pady=5)
+            pseudo_entry = CTkEntry(window_info02, font=("Arial", 15))
+            # pseudo_entry.pack(ipadx=2, ipady=10, padx=5,pady=5)
+            pseudo_entry.grid(row=1, column=1)
+
+            lbl_result = CTkLabel(window_info02, text=f"{pseudo}  Essais réussis : 0/0", font=("Arial", 15))
+            lbl_result.grid(row=1, column=2, columnspan=3, ipady=5, padx=20, pady=20)
+
+            label_n1 = CTkLabel(window_info02, text="n1:", font=("Arial", 15))
+            label_n1.grid(row=2, column=0, ipady=5, padx=20, pady=20, sticky='E')
+
+            entry_n2 = CTkEntry(window_info02, font=("Arial", 15))
+            entry_n2.grid(row=2, column=1, ipady=5, padx=5, pady=20, sticky='E')
+
+            label_u2 = CTkLabel(window_info02, text="u2:", font=("Arial", 15))
+            label_u2.grid(row=2, column=2, ipady=5, padx=5, pady=20, sticky='W')
+
+            btn_next = CTkButton(window_info02, text="Suivant", font=("Arial", 15))
+            btn_next.grid(row=3, column=0, columnspan=3, ipady=5, padx=5, pady=5)
+
+            btn_finish = CTkButton(window_info02, text="Terminer", font=("Arial", 15))
+            btn_finish.grid(row=6, column=0, columnspan=6)
+
+            # button-1 clic a gauche souris
+            # bind pour relier un function sur un bouton
+            btn_finish.bind("<Button-1>", lambda event: (save_game(event)))
+
+            start_date = datetime.datetime.now()
+            display_timer()
+            # first call of next_point
+            next(event=None)
+
+
+            # binding actions (entry & buttons)
+            entry_n2.bind("<Return>", test)
+            btn_next.bind("<Button-1>", next)
+            btn_finish.bind("<Button-1>", save_game)
+
+            # Main loop
+            window_info02.mainloop()
