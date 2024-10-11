3d8893c5c9f93b655f396a7f975b6d150afc69d5
Nico Mengisen <mengisennico@gmail.com>
2024-10-11 10:44:01 +0200

    Refactor file paths and update menu function call

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import new_menu as menu
+
 # Get the absolute path of the current script
 current_path = os.path.dirname(os.path.abspath(__file__))
 
 # Append the 'src' folder to the sys.path
-src_path = os.path.join(current_path, 'src')
+src_path = os.path.join(current_path, '..', 'src')
 sys.path.append(src_path)
 
-menu.launch_menu()
+menu()
