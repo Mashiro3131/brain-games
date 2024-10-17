5751685ed92608347d7e4303b2da7d5512fac00e
Nico Mengisen <mengisennico@gmail.com>
2024-10-17 21:43:06 +0200

    Update - Updated database structure

--- database/braingames_structure.sql
+++ database/braingames_structure.sql
@@ -2,24 +2,24 @@
 -- Structure of database creatoion and usage `braingames_db`
 --
 
-CREATE DATABASE IF NOT EXISTS `braingames_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `braingames_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE `braingames_db`;
-
-
 
 --
 -- Structure of table `results`
 --
 
-CREATE TABLE `results` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `exercise` varchar(50) NOT NULL,
-  `date_hour` datetime NOT NULL,
-  `duration` time NOT NULL,
-  `nbtrials` int(11) NOT NULL,
-  `nbok` int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `exercise` VARCHAR(50) NOT NULL,
+  `date_hour` DATETIME NOT NULL,
+  `duration` TIME NOT NULL,
+  `nbtrials` INT(11) NOT NULL,
+  `nbok` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
 
@@ -28,9 +28,9 @@ CREATE TABLE `results` (
 -- Structure of table `roles`
 --
 
-CREATE TABLE `roles` (
-  `id` int(11) PRIMARY KEY NOT NULL,
-  `name` varchar(50) NOT NULL
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` INT(11) PRIMARY KEY NOT NULL,
+  `name` VARCHAR(50) NOT NULL
 );
 
 
@@ -39,19 +39,11 @@ CREATE TABLE `roles` (
 -- Structure of table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `pseudo` varchar(50) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `role_id` int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `role_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
 );
-
-
-
---
--- Structure of constraints
---
-
-ALTER TABLE `results` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
